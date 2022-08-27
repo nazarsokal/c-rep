5b9022a2cec05518de27f9209e92ce93c3b36062
nazar <nazarsokalch@gmail.com>
2022-08-27 01:39:00 -0700

    commit

---
+++ project_git/project_git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project_git
+//
+//  Created by abb on 27.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
